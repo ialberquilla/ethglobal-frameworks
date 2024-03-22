2ccbbeafc69c66baa3cb3e3387c6f510fcb78ccb
Ivan <ivan.alberquilla@gmail.com>
2024-03-22 18:54:47 +0100

    conditional

--- frame/app/api/frame/route.ts
+++ frame/app/api/frame/route.ts
@@ -1,61 +1,43 @@
-import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { FrameRequest, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
-  if (!isValid) {
-    return new NextResponse('Message not valid', { status: 500 });
-  }
+  const meet = false;
 
-  const text = message.input || '';
-  let state = {
-    page: 0,
-  };
-  try {
-    state = JSON.parse(decodeURIComponent(message.state?.serialized));
-  } catch (e) {
-    console.error(e);
-  }
-
-  /**
-   * Use this code to redirect to a different page
-   */
-  if (message?.button === 3) {
-    return NextResponse.redirect(
-      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-      { status: 302 },
-    );
-  }
-
-  return new NextResponse(
-    getFrameHtmlResponse({
-      buttons: [
-        {
-          label: `State: ${state?.page || 0}`,
-        },
-        {
-          action: 'link',
-          label: 'OnchainKit',
-          target: 'https://onchainkit.xyz',
-        },
-        {
-          action: 'post_redirect',
-          label: 'Dog pictures',
-        },
-      ],
-      image: {
-        src: `${NEXT_PUBLIC_URL}/park-1.png`,
+  const responseMeetCriteria = getFrameHtmlResponse({
+    buttons: [
+      {
+        label: `Buy now`,
       },
-      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      state: {
-        page: state?.page + 1,
-        time: new Date().toISOString(),
+    ],
+    image: {
+      src: `${NEXT_PUBLIC_URL}/park-1.png`,
+    },
+    postUrl: `${NEXT_PUBLIC_URL}/api/buy`,
+    state: {
+      meet: true,
+    },
+  });
+
+  const responseDoesNotMeetCriteria = getFrameHtmlResponse({
+    buttons: [
+      {
+        label: `Buy now`,
       },
-    }),
-  );
+    ],
+    image: {
+      src: `${NEXT_PUBLIC_URL}/park-2.png`,
+    },
+    postUrl: `${NEXT_PUBLIC_URL}/api/buy`,
+    state: {
+      meet: false,
+    },
+  });
+
+  return new NextResponse(meet ? responseMeetCriteria : responseDoesNotMeetCriteria);
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
