7ca491eef39937dd339fc050229022cd0451eb57
Ivan <ivan.alberquilla@gmail.com>
2024-03-22 19:05:51 +0100

    buy path

--- frame/app/config.ts
+++ frame/app/config.ts
@@ -1,4 +1,7 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'https://d225-213-99-159-252.ngrok-free.app' : 'https://zizzamia.xyz';
+  process.env.NODE_ENV == 'development'
+    ? 'https://d225-213-99-159-252.ngrok-free.app'
+    : 'https://zizzamia.xyz';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+export const NEYNAR_API_KEY = process.env.NEXT_PUBLIC_NEYNAR_API_KEY || '';
